235242f94c2bc0e48bad7fc33c82faba1fa5b3d7
studioswong <studioswong@gmail.com>
2021-05-26 15:12:02 +0100

    set up demo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import KedroViz from "@quantumblack/kedro-viz";
+import animals from "@quantumblack/kedro-viz/lib/utils/data/animals.mock.json";
+import demo from "@quantumblack/kedro-viz/lib/utils/data/demo.mock.json";
+import getRandomData from "@quantumblack/kedro-viz/lib/utils/random-data";
 
-function App() {
+export const dataSources = {
+  animals: () => animals,
+  demo: () => demo,
+  random: () => getRandomData(),
+};
+
+const Radio = ({ current, value, onChange }) => (
+  <label>
+    <input
+      type="radio"
+      name="data"
+      value={value}
+      onChange={onChange}
+      checked={value === current}
+    />
+    {value}
+  </label>
+);
+
+const App = ({ initialData }) => {
+  const [dataKey, updateDataKey] = useState(initialData);
+  const onChange = (e) => updateDataKey(e.target.value);
+
+  const visibleSetting = { sidebar: false };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "80vh" }}>
+      <p>
+        Data source:
+        <Radio value="random" onChange={onChange} current={dataKey} />
+        <Radio value="animals" onChange={onChange} current={dataKey} />
+        <Radio value="demo" onChange={onChange} current={dataKey} />
+      </p>
+      <KedroViz data={dataSources[dataKey]()} visible={visibleSetting} />
     </div>
   );
-}
+};
+
+App.defaultProps = {
+  initialData: "random",
+};
 
 export default App;

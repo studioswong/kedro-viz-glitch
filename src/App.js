917c857ce94eff829b0f099a483993f4468e705d
datajoely <joel.schwarzmann@quantumblack.com>
2021-06-28 10:58:34 +0100

    Add extenral structure

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
-import React, { useState } from "react";
 import KedroViz from "@quantumblack/kedro-viz";
-import animals from "@quantumblack/kedro-viz/lib/utils/data/animals.mock.json";
-import demo from "@quantumblack/kedro-viz/lib/utils/data/demo.mock.json";
-import getRandomData from "@quantumblack/kedro-viz/lib/utils/random-data";
+import * as data from "./structure.json"
+
+
 
 export const dataSources = {
-  animals: () => animals,
-  demo: () => demo,
-  random: () => getRandomData(),
+  
+  layers: () => data
 };
 
-const Radio = ({ current, value, onChange }) => (
-  <label>
-    <input
-      type="radio"
-      name="data"
-      value={value}
-      onChange={onChange}
-      checked={value === current}
-    />
-    {value}
-  </label>
-);
+console.log(data)
 
 const App = ({ initialData }) => {
-  const [dataKey, updateDataKey] = useState(initialData);
-  const onChange = (e) => updateDataKey(e.target.value);
 
   const visibleSetting = { sidebar: false };
 
   return (
-    <div style={{ height: "80vh" }}>
-      <p>
-        Data source:
-        <Radio value="random" onChange={onChange} current={dataKey} />
-        <Radio value="animals" onChange={onChange} current={dataKey} />
-        <Radio value="demo" onChange={onChange} current={dataKey} />
-      </p>
-      <KedroViz data={dataSources[dataKey]()} visible={visibleSetting} />
+    <div style={{ height: "100vh" }}>
+      <KedroViz data={dataSources["layers"]()} visible={visibleSetting} />
     </div>
   );
 };
 
 App.defaultProps = {
-  initialData: "random",
+  initialData: "layers",
 };
 
 export default App;

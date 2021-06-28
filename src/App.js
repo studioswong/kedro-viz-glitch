7e685ce84fb815141244d41dc0ebef283f698004
datajoely <joel.schwarzmann@quantumblack.com>
2021-06-28 11:42:40 +0100

    Switch to different data sources

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import KedroViz from "@quantumblack/kedro-viz";
-import * as dataStructure from "./structure.json";
+import * as sourceDomainModel from "./data/source_domain_model.json"
+import * as representativePipeline from "./data/complete_demo_pipeline.json"
 
 const App = ({ initialData }) => {
-  const visibleSetting = { sidebar: false, miniMap: false };
+const visibleSetting = { sidebar: false, miniMap: false };
+const dataSources = {
+  sourceDomain : sourceDomainModel,
+  complete : representativePipeline
+}
 
   return (
     <div style={{ height: "100vh" }}>
-      <KedroViz data={dataStructure.default} visible={visibleSetting} />
+      <KedroViz data={dataSources.sourceDomainModel} visible={visibleSetting} />
     </div>
   );
 };
